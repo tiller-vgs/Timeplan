dcc5fe2ebe93e2ab6916555b9a3516314fc45836
Showdowncode <konradbottengranas@gmail.com>
2024-04-22 13:56:29 +0200

    Add code and remove code.

---
+++ timeplan/app/schedule/page.jsx
@@ -0,0 +1,10 @@
+export default function Schedule() {
+    return (
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br text-white from-slate-900 to-slate-800">
+        <div className="flex flex-col items-center gap-y-10">
+          <h1 className="text-8xl">Timeplan</h1>
+          
+        </div>
+      </main>
+    );
+  }
