f773354d096df6b5316f2ee21dc2579c34bba61e
OlaBekkevold <ola.k.bekkevold@gmail.com>
2024-04-22 14:23:45 +0200

    feat: Add link to schedule page
    Add the Link component to add a link to the schedule directory
    
    Refs: #2

--- timeplan/app/page.js
+++ timeplan/app/page.js
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br text-white from-slate-900 to-slate-800">
-      <div className="flex flex-col items-center gap-y-10">
+
         <h1 className="text-8xl">Hjemmeside</h1>
-        
-      </div>
+
+
+        <div>
+            <ul>
+                <li>
+                    <Link href="/schedule">1IM Timeplan</Link>
+                </li>
+            </ul>
+        </div>
     </main>
   );
 }
